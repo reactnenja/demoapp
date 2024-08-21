39c1e2f8cb74c40b5b6a62d2f33dd4ca5ad36523
Ulugbek Eshnazarov (reactnenja) <reactnenja@gmail.com>
2024-08-21 14:55:24 +0100

    change

--- src/app/dashboard/(components)/rounededData/RoundedData.tsx
+++ src/app/dashboard/(components)/rounededData/RoundedData.tsx
@@ -121,7 +121,7 @@ export function RoundedData() {
             </CardContent>
             <CardFooter className="flex-col gap-2 text-sm">
                 <div className="flex items-center gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month{" "}
+                    Trending up by 5.2% this month
                     <TrendingUp className="h-4 w-4" />
                 </div>
                 <div className="leading-none text-muted-foreground">

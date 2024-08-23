b6954dfa216913347abf3544bb5013d5bd9ccb60
Ulugbek Eshnazarov (reactnenja) <reactnenja@gmail.com>
2024-08-23 09:53:27 +0100

    full apdate login page add react-icons google, and github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,55 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import {
-    LoginLink,
-    RegisterLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
     return (
         <div className="flex justify-center items-center min-h-screen w-full">
-            <div className="bg-white rounded-md shadow-md w-[480px] h-[200px] flex justify-center items-center border-2 flex-col gap-2">
-                <LoginLink
-                    postLoginRedirectURL="/dashboard"
-                    className="bg-black text-white inline-block w-[400px] text-center shadow-md ml-4 px-8 py-2 rounded-md"
-                >
-                    Sign in
-                </LoginLink>
-                <RegisterLink
-                    postLoginRedirectURL="/welcome"
-                    className="bg-black text-white inline-block w-[400px] text-center shadow-md ml-4 px-8 py-2 rounded-md"
-                >
-                    Sign up
-                </RegisterLink>
+            <div className=" flex justify-center items-center gap-2">
+                <Card className="w-[400px] h-[400px]">
+                    <CardHeader className="text-center">
+                        <CardTitle className="text-center text-xl">
+                            Login
+                        </CardTitle>
+                        <CardDescription>
+                            Enter your email and password
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="border-b">
+                        <form action="">
+                            <div className="space-y-2 py-4">
+                                <Label htmlFor="email">Email</Label>
+                                <Input type="email" placeholder="Your email" />
+                            </div>
+                            <Button className="w-full">Login</Button>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="w-full py-2 flex gap-3 flex-col">
+                        <Button className="w-full flex gap-2">
+                            <span>
+                                <FcGoogle className="h-5 w-5" />
+                            </span>
+                            <span>Google</span>
+                        </Button>
+                        <Button className="w-full flex gap-2">
+                            <span>
+                                <FaGithub className="h-5 w-5" />
+                            </span>
+                            <span>GitHub</span>
+                        </Button>
+                    </CardFooter>
+                </Card>
             </div>
         </div>
     );

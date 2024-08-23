753bddce1c2074833387cd6a03dc268e06a1a1c4
Ulugbek Eshnazarov (reactnenja) <reactnenja@gmail.com>
2024-08-23 18:27:42 +0500

    full frist commit

--- src/app/dashboard/developers/page.tsx
+++ src/app/dashboard/developers/page.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 const Developers = () => {
     return (
         <div className="fixed top-20 left-20 w-[calc(100%-80px)]  p-4">
-            Commits
+            <div className="w-full border-b-2 sm:w-[450px] md:w-[450px] lg:w-[450px] border-solid p-2 capitalize text-center">
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/dashboard">
+                                home
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/dashboard/developers">
+                                developers
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
         </div>
     );
 };
